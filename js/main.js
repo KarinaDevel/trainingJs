cb8fa8a440bc21d5e8aab0885cede32a87cc3c1c
KARINA <kgrib7373@gmail.com>
2024-11-24 21:00:26 +0100

    feat: improved front view + appended spots functional

--- js/main.js
+++ js/main.js
@@ -14,3 +14,40 @@ function closeBurger(){
     const list = document.getElementById('list');
     list.style.display = "none"
 }
+
+
+
+const left = document.getElementById('btnLeft');
+const right = document.getElementById('btnRight');
+const slides = document.querySelectorAll('.sliderImg');
+const bottom = document.querySelectorAll('.bottom .circle')
+
+let currentSlide = 0;
+function showSlide() {
+    slides[currentSlide].classList.add('block')
+    if (bottom[currentSlide]) {
+        bottom[currentSlide].classList.add('active');
+    }
+}
+function hideSlide(){
+    slides[currentSlide].classList.remove("block");
+    if (bottom[currentSlide]) {
+        bottom[currentSlide].classList.remove('active');
+    }
+}
+function nextSlide() {
+    hideSlide()
+    currentSlide++;
+    if (currentSlide > slides.length - 1) {
+        currentSlide = 0;
+    }
+    showSlide()
+}
+function previousSlide(){
+    hideSlide();
+    currentSlide--;
+    if (slides[currentSlide] < 0){
+        currentSlide = slides.length - 1;
+    }
+    showSlide()
+}
